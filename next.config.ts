b8f099b06cd49b305f1e0f8390b96f3bcd0ebd41
Kevin Tale <kevin.taleb.dehkordi@hotmail.com>
2026-08-22 13:15:32 +0200

    Allow Next.js preview on 127.0.0.1

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
   basePath: process.env.PAGES_BASE ?? "",
+  allowedDevOrigins: ["127.0.0.1"],
 };
 
 export default nextConfig;
